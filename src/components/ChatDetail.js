9db11f312ae0332589df466c3b3113fbafa837a9
Manuel Concepción Brito <manu041196@gmail.com>
2021-12-22 17:50:17 +0000

    remove unused code

--- src/components/ChatDetail.js
+++ src/components/ChatDetail.js
@@ -1,29 +1,3 @@
-// import React, {useState, useEffect} from "react";
-// import 'firebase/firestore'
-// import 'firebase/auth'
-// import {db} from '../firestore/firestore.js'
-// import { collection, query, onSnapshot } from "firebase/firestore";
-//
-// import ChatMessage from './ChatMessage'
-// import {useCollectionData} from "react-firebase-hooks/firestore";
-
-// export default function ChatDetail() {
-//     const [messages, setMessages] = useState([])
-//     useEffect(() => {
-//         const q = query(collection(db, 'messages'))
-//         const messagesSnap = onSnapshot(q, (querySnapshot) => {
-//             const messages = querySnapshot.docs.map(doc => doc.data())
-//             setMessages(messages)
-//         })
-//     }, [])
-//
-//     return (
-//         <div>
-//             {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg.text}/>)}
-//         </div>
-//     )
-// }
-
 import React, {useState, useEffect} from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Paper } from "@material-ui/core";
