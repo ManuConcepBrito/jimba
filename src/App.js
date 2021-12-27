0a9ca97dad72957508752a4554be710b8d6e7452
Manuel Concepción Brito <manu041196@gmail.com>
2021-12-27 11:09:50 +0000

    Re-usable switch

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import SignUp from "./components/SignUp";
 import Chats from "./components/ChatEngine";
 import AssetDetail from "./components/AssetDetail";
 import VisualProof from "./components/VisualProof/VisualProof";
-import Header from "./components/Header";
+import CustomSwitch from "./components/CustomSwitch";
 
 const theme = createTheme({
   typography: {
@@ -46,7 +46,6 @@ function App() {
           <Route path="/car-list" element={<CarList/>} />
           <Route path="/asset/:uid" element={<AssetDetail/>} />
           <Route path="/proof" element={<VisualProof/>} />
-          <Route path="/header" element={<Header/>} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/components/CustomSwitch.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import {styled} from "@mui/material/styles";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import {Switch} from "@material-ui/core";
+import Box from "@mui/material/Box";
+
+const Item = styled(Paper)(({theme}) => ({
+    ...theme.typography.body2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 40,
+    lineHeight: '40px',
+    padding:8,
+}));
+
+function CustomSwitch(props) {
+    return (
+
+        <Grid
+            container
+        >
+            <Box
+                m={2}
+            >
+                <Item
+                    elevation={3}
+                >
+
+                    <FormGroup>
+                        <FormControlLabel control={<Switch color="primary"/>} labelPlacement="start"
+                                          label={props.title}/>
+
+                    </FormGroup>
+
+
+                </Item>
+            </Box>
+
+
+        </Grid>
+
+    );
+}
+
+export default CustomSwitch;
